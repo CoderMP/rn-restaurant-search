aac59cfc4c0ff995b28b8832d706aa326503dd57
Mark Philips <mphilips15@gmail.com>
2021-03-29 20:57:42 -0400

    Added state hook to SearchScreen.js and state-monitoring code, props, and callbacks to SearchBar and SearchScreen, completed styling for the SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,49 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-const SearchBar = () => {
+/**
+ * Function that is responsible for creating the SearchBar reusable component
+ * for the application.
+ * 
+ * @returns The SearchBar reusable component
+ * @author Mark Philips
+ */
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
     return (
-        <View style={styles.barBackground}>
-            <Text>Search Bar</Text>
+        <View style={ styles.backgroundStyle }>
+            <MaterialIcons name="search" style={ styles.iconStyle }/>
+            <TextInput 
+                placeholder="Search" 
+                style={ styles.textinputStyle }
+                autoCapitalize="none"
+                autoCorrect={ false }
+                value={ term }
+                onChangeText={ onTermChange }
+                onEndEditing={ onTermSubmit }/>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    barBackground: {
+    backgroundStyle: {
         backgroundColor: '#d3d4d8',
         height: 50,
         borderRadius: 5,
         marginTop: 10,
         marginHorizontal: 15,
-    }
+        flexDirection: 'row',
+    },
+    iconStyle: {
+        margin: 5,
+        color: 'black',
+        fontSize: 40,
+        alignSelf: 'center',
+    },
+    textinputStyle: {
+        flex: 1,
+        fontSize: 18,
+    },
 });
 
 export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+    // State variable to track the current search term
+    const [searchTerm, setSearchTerm] = useState('');
+
     return (
         <View>
-            <SearchBar />
+            <SearchBar 
+                term={ searchTerm } 
+                onTermChange={newSearchTerm => setSearchTerm(newSearchTerm)}
+                onTermSubmit={() => console.log('term was submitted')}/>
             <Text>Search Screen</Text>
+            <Text>{searchTerm}</Text>
         </View>
     );
 };
