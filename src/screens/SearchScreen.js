37e8c7c142190bedd1c9b0a0196563dcb1660ddb
Mark Philips <mphilips15@gmail.com>
2021-03-31 16:35:32 -0400

    Added Axios package for async API calls, Added yelp.js file for making Axios API calls, Completed inital API async call implementation in SearchScreen.js

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,43 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
-    // State variable to track the current search term
-    const [searchTerm, setSearchTerm] = useState('');
+    // State variables to track the current search term and 
+    // (possible) results from the API call
+    const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMsg, setErrorMsg] = useState('');
+
+    /**
+     * Function that is responsible for making an async call to the API
+     */
+    const searchApi = async () => {
+        try {
+            const response = await yelp.get('/search', { 
+                params: { 
+                    term: term,
+                    location: 'toronto',
+                    limit: 50
+                }
+            });
+    
+            // Set any results that are retrieved from the API call to the state
+            setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMsg('Something went wrong');
+        }
+    };
 
     return (
         <View>
             <SearchBar 
-                term={ searchTerm } 
-                onTermChange={newSearchTerm => setSearchTerm(newSearchTerm)}
-                onTermSubmit={() => console.log('term was submitted')}/>
-            <Text>Search Screen</Text>
-            <Text>{searchTerm}</Text>
+                term={ term } 
+                onTermChange={ setTerm }
+                onTermSubmit={ searchApi }/>
+            {errorMsg ? <Text>{errorMsg}</Text> : null}
+            <Text>We have found {results.length} results</Text>
         </View>
     );
 };
